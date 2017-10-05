5f772bc9792dff2ff0ee819a5704496cc2022989
Duy DANG <tao.zen.duy@gmail.com>
2017-10-05 14:16:45 +0200

    Rediriger sur la page de resultats de la recherche Google

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 
 let monBouton = document.querySelector('button');
 
+// Créer un lien avec l'objet bouton
+let monBoutonSearch = document.querySelector('#rech');
+
+// Texte de recherche
+let monUrl;
+
 // je cache mes éléments
 document.getElementById('corps').style.display='none';
 
 // au clic je veux que le block apparaisse
 monBouton.onclick = function() {
 document.getElementById('corps').style.display='flex';
-}
+};
+
+
+// si clique sur bouton recherche google
+monBoutonSearch.onclick = function() {
+
+// récupérer le texte tapé
+monUrl = document.getElementById("search").value;
+
+// changer l'adresse de l url
+window.location.href="https://www.google.fr/search?q=" + monUrl;
+
+};
 
 
