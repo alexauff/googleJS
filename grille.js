38d8fdc02003d6612e0c7117e80041ab47cc28fb
juliette <ju.auvray@orange.fr>
2017-10-05 17:08:33 +0200

    début grille

--- grille.js
+++ grille.js
@@ -1,4 +1,18 @@
 
-setTimeout(function() {
-let monNombre = prompt('Indiquez la taille de la grille (entre 1 et 70):');
-  localStorage.setItem('nombre', monNombre);}, 0);
+// ouvrir la fenêtre de demande et récupération du nombre donné par l'utilisateur
+let N; 
+let chaineligne="";
+let chaine="";
+N = parseInt(prompt('Indiquez la taille de la grille (entre 1 et 70):'));
+
+
+/* pour conserver l'input utilisateur dans l'historique 
+localStorage.setItem('nombre', N);*/
+
+//rajout de la variable pour faire une boucle
+for (let i=0; i<N; i++){chaineligne=chaineligne+"<carre></carre>";}
+for (let i=0; i<N; i++){chaine=chaine+"<div>"+chaineligne+"</div>";}
+chaine ="<div><div>" + chaine + "</div></div>";
+document.getElementById('grille').innerHTML =chaine;
+
+
