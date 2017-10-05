731808ddf2a1743d92aa6f0e4e0bd089401f84ab
Duy DANG <tao.zen.duy@gmail.com>
2017-10-05 17:37:09 +0200

    Grille avec carre fixe

--- grille.js
+++ grille.js
@@ -1,18 +1,24 @@
 
 // ouvrir la fenêtre de demande et récupération du nombre donné par l'utilisateur
-let N; 
+let N;
 let chaineligne="";
 let chaine="";
-N = parseInt(prompt('Indiquez la taille de la grille (entre 1 et 70):'));
+let chaine2="";
+let demander = true
+
+while(demander){
+  N = parseInt(prompt('Indiquez la taille de la grille (entre 1 et 70):'));
+  demander = (N<1 || N> 70);
+}
 
 
-/* pour conserver l'input utilisateur dans l'historique 
+/* pour conserver l'input utilisateur dans l'historique
 localStorage.setItem('nombre', N);*/
 
 //rajout de la variable pour faire une boucle
-for (let i=0; i<N; i++){chaineligne=chaineligne+"<carre></carre>";}
+for (let i=0; i<N; i++){chaineligne=chaineligne+"<carre>O</carre>";}
 for (let i=0; i<N; i++){chaine=chaine+"<div>"+chaineligne+"</div>";}
-chaine ="<div><div>" + chaine + "</div></div>";
-document.getElementById('grille').innerHTML =chaine;
+chaine ="<div>" + chaine + "</div>";
+document.getElementById('grille').innerHTML = chaine;
 
 
